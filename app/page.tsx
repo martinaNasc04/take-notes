0981fc608b0655189cb1b2817a80b4336bc3dbc4
Martina Nascimento <martinanascimento95@gmail.com>
2025-11-24 18:59:34 -0300

    Add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import AboutSection from "@/components/sections/About";
 import HeroSection from "@/components/sections/Hero";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <div className="min-h-screen bg-white">
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </div>
 
   );
---
+++ components/sections/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AboutSection = () => {
+    return (
+        <section id='about' className="min-h-screen flex items-center justify-center relative mx-8">
+            <div className="flex gap-10 items-center  justify-center md:flex-col md:mt-20">
+                <h2 className='md:text-6xl font-serif'>About Take Notes</h2>
+                <p className='max-w-xl text-lg mt-4 text-justify text-gray-700'>
+                    Take Notes is a simple and intuitive note-taking application designed to help you capture your ideas, 
+                    inspirations, and important information effortlessly. Whether you&apos;re a student, 
+                    professional, or just someone who loves to jot down thoughts, 
+                    Take Notes provides a clean and distraction-free environment for all your note-taking needs.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default AboutSection
